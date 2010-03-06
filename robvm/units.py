0c29d5fab92afbf7a9eccfa93f0c908fa0fe32d1
Marinho Brandão <marinho@gmail.com>
2010-03-06 09:18:22 -0300

    Just playing with more calculations

--- robvm/units.py
+++ robvm/units.py
@@ -38,6 +38,7 @@ class Value(Decimal):
         # Stores the metry unit
         obj = Decimal.__new__(cls, value)
         obj.unit = unit
+        obj.exp = exp
         
         return obj
 
@@ -45,7 +46,7 @@ class Value(Decimal):
         return self.as_string()
 
     def as_string(self):
-        exp = self.exp != '1' and self.exp or ''
+        exp = self.exp != '1' and 'x'+self.exp or ''
         unit = self.unit != unknown and self.unit or ''
 
         return ('%s%s %s'%(self, exp, unit)).strip()
@@ -55,7 +56,7 @@ class Value(Decimal):
             raise InvalidValue('Invalid unit. Should be coulomb and is '+self.unit)
 
         val = Value(self * Decimal('6.242'))
-        val.exp = 'x10^18'
+        val.exp = '10^18'
 
         return val
 
@@ -98,6 +99,7 @@ class Value(Decimal):
         result = super(Value, self).__mul__(value)
         unit = self.unit
 
+        # Defines the unit
         if isinstance(value, Value):
             # Q = t * I
             if self.unit == second and value.unit == ampere:
@@ -110,6 +112,22 @@ class Value(Decimal):
             # V = R * I
             if self.unit == ohm and value.unit == ampere:
                 unit = volt
+
+            # Exponential
+            exp1 = self.exp.split('^')
+            exp2 = value.exp.split('^')
+            exp = str(Decimal(exp1[0])) # + Decimal(exp2[0]))
+            if len(exp1) > 1 and len(exp2) > 1:
+                exp += '^'+str(Decimal(exp1[1]) + Decimal(exp2[1]))
+            elif len(exp1) > 1:
+                exp += '^'+Decimal(exp1[1])
+            elif len(exp2) > 1:
+                exp += '^'+Decimal(exp2[1])
+        else:
+            exp = self.exp
         
-        return Value('%s %s'%(result, unit))
+        val = Value('%s %s'%(result, unit))
+        val.exp = exp
+
+        return val
 
